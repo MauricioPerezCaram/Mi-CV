6eb54718989c4f04303855876350ba4efca6506f
Mauricio Perez Caram <perezcarammauricio@gmail.com>
2023-11-25 21:11:39 -0300

    proyectos

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -4,22 +4,34 @@ const Proyectos = () => {
       <h2>Estos son algunos proyectos en los que he trabajado</h2>
       <div className="container-proyectos">
         <div className="proyecto">
-          <img src="" alt="" />
+          <img src="/fotocv.png" alt="" />
+          <h3>Landing Page</h3>
+          <h4>Estudio Jurídico Pérez Caram & Asociados</h4>
+          <h5>Tecnologías empleadas:</h5>
+          <ul>
+            <li>HTML</li>
+            <li>SASS</li>
+          </ul>
+        </div>
+        <div className="proyecto">
+          <img src="/fotocv.png" alt="" />
+          <h3>Ecommerce</h3>
+          <h4>Apple Store Mendoza</h4>
+          <h5>Tecnologías empleadas:</h5>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>REACT</li>
+            <li>FIREBASE</li>
+          </ul>
+        </div>
+        <div className="proyecto">
+          <img src="/fotocv.png" alt="" />
           <h3>Estudio Jurídico Pérez Caram & Asociados</h3>
           <h4>Landing page de estudio jurídico</h4>
         </div>
         <div className="proyecto">
-          <img src="" alt="" />
-          <h3>Estudio Jurídico Pérez Caram & Asociados</h3>
-          <h4>Landing page de estudio jurídico</h4>
-        </div>
-        <div className="proyecto">
-          <img src="" alt="" />
-          <h3>Estudio Jurídico Pérez Caram & Asociados</h3>
-          <h4>Landing page de estudio jurídico</h4>
-        </div>
-        <div className="proyecto">
-          <img src="" alt="" />
+          <img src="/fotocv.png" alt="" />
           <h3>Estudio Jurídico Pérez Caram & Asociados</h3>
           <h4>Landing page de estudio jurídico</h4>
         </div>
