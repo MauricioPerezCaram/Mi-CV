dd68f5d9c162d40c814d775b2935c6e47262a7ff
MauricioPerezCaram <perezcarammauricio@gmail.com>
2023-11-30 21:13:36 -0300

    css

--- src/Components/Proyectos.jsx
+++ src/Components/Proyectos.jsx
@@ -1,27 +1,23 @@
 const Proyectos = () => {
-
   const abrirpaginaestudio = () => {
-    window.open('https://primer-subida-githhub-pre-entrega.vercel.app/', '_blank');
+    window.open(
+      "https://primer-subida-githhub-pre-entrega.vercel.app/",
+      "_blank"
+    );
   };
 
-  const abrirapplestore= () => {
-    window.open('https://entrega-final-react-perez-caram.vercel.app/');
-  }
+  const abrirapplestore = () => {
+    window.open("https://entrega-final-react-perez-caram.vercel.app/");
+  };
+
+  const abrirtodoapp = () => {
+    window.open("https://to-do-app-one-omega.vercel.app/");
+  };
 
   return (
-    <section id="proyectos" >
+    <section id="proyectos">
       <h2 className="proyectos">Proyectos en los que he trabajado</h2>
       <div className="container-proyectos">
-        <div className="proyecto" onClick={abrirpaginaestudio}>
-          <img src="Estudio Jco. PC & Asoc.png" alt="Estudio Jco. PC & Asoc" />
-          <h3>Landing Page</h3>
-          <h4>Pérez Caram & Asociados</h4>
-          <h5>Tecnologías empleadas:</h5>
-          <ul>
-            <li>HTML</li>
-            <li>SASS</li>
-          </ul>
-        </div>
         <div className="proyecto" onClick={abrirapplestore}>
           <img src="Apple Store Mendoza.png" alt="Apple Store Mendoza" />
           <h3>E-commerce</h3>
@@ -34,7 +30,38 @@ const Proyectos = () => {
             <li>FIREBASE</li>
           </ul>
         </div>
-       
+        <div className="proyecto" onClick={abrirtodoapp}>
+          <img src="to do app.png" alt="to do app" />
+          <h3>To Do App</h3>
+          <h5>Tecnologías empleadas:</h5>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>REACT</li>
+          </ul>
+        </div>
+        <div className="proyecto" onClick={abrirpaginaestudio}>
+          <img src="Estudio Jco. PC & Asoc.png" alt="Estudio Jco. PC & Asoc" />
+          <h3>Landing Page</h3>
+          <h4>Pérez Caram & Asociados</h4>
+          <h5>Tecnologías empleadas:</h5>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SASS</li>
+          </ul>
+        </div>
+        <div className="proyecto" onClick={abrirpaginaestudio}>
+          <img src="Estudio Jco. PC & Asoc.png" alt="Estudio Jco. PC & Asoc" />
+          <h3>Landing Page</h3>
+          <h4>Pérez Caram & Asociados</h4>
+          <h5>Tecnologías empleadas:</h5>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SASS</li>
+          </ul>
+        </div>
       </div>
     </section>
   );
--- src/Components/SobreMi.jsx
+++ src/Components/SobreMi.jsx
@@ -2,20 +2,21 @@ const SobreMi = () => {
   return (
     <section id="sobremi" className="sobremi">
       <h2 className="tecnologias-desarrollo">Sobre mi</h2>
-      <h3>
-        Hola soy Mauri! Me encanta el desarrollo de software, la programación y tecnología, es por
-        eso que me encuentro cursando la carrera de desarrollador Full Stack.
-      </h3>
-      <h3>
+      <p>
+        Hola soy Mauri! Me encanta el desarrollo de software, la programación y
+        tecnología, es por eso que me encuentro cursando la carrera de
+        desarrollador Full Stack.
+      </p>
+      <p>
         He aprendido Desarrollo Web aplicando HTML y CSS. Luego comencé a
         estudiar Javascript donde logré darle funcionalidad e interacción a mis
         proyectos. Para completar Desarrollo Frontend estudié React donde
         aprendí la programación a base a componentes.
-      </h3>
-      <h3>
+      </p>
+      <p>
         Hoy con muchas ganas de seguir aprendiendo me encuentro estudiando
         Backend donde aprendo día a día la programación del lado del servidor.
-      </h3>
+      </p>
     </section>
   );
 };
